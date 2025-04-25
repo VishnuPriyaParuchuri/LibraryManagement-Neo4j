8cb142712f92fd277522c25ec338430b1253d979
VishnuPriyaParuchuri <priyaparuchuri111@gmail.com>
2025-04-25 16:59:08 +0530

    Initial commit

---
+++ src/main/java/com/libraryManagement/neo4j/LibraryManagementNeo4jApplication.java
@@ -0,0 +1,13 @@
+package com.libraryManagement.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementNeo4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libraryManagement/neo4j/LibraryManagementNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryManagement.neo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementNeo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
